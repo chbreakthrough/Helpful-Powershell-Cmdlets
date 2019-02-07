248752e1df7f70db66e8f5aed7f649b0092a2d3f
Chris Hamilton <christopher.m.hamilton26@gmail.com>
2019-02-07 16:56:14 -0500

    Update Remove_Bloat_Windows.ps1
    Adding Some functionality to remove some of the apps included with my ASUS laptop.

--- Remove_Bloat_Windows.ps1
+++ Remove_Bloat_Windows.ps1
@@ -1,4 +1,4 @@
-﻿#title           :Remove_Bloat_Windows.ps1
+#title           :Remove_Bloat_Windows.ps1
 #description     :Removes unuseful apps on Windows 10
 #author		     :ChBreakthrough
 #date            :2010829
@@ -38,3 +38,32 @@ Get-AppxPackage king.com.BubbleWitch3Saga | Remove-AppxPackage
 Get-AppxPackage king.com.CandyCrushSodaSaga | Remove-AppxPackage
 Get-AppxPackage A278AB0D.MarchofEmpires| Remove-AppxPackage
 Get-AppxPackage Microsoft.MinecraftUWP | Remove-AppxPackage
+Get-AppxPackage B9ECED6F.MyASUS | Remove-AppxPackage
+Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage *acg* | Remove-AppxPackage
+Get-AppxPackage *alarms* | Remove-AppxPackage
+Get-AppxPackage *calculator* | Remove-AppxPackage
+Get-AppxPackage *communications* | Remove-AppxPackage
+Get-AppxPackage *camera* | Remove-AppxPackage
+Get-AppxPackage *dolbyaccess* | Remove-AppxPackage
+Get-AppxPackage *fitbitcoach* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage *maps* | Remove-AppxPackage
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *phototastic* | Remove-AppxPackage
+Get-AppxPackage *photos* | Remove-AppxPackage
+Get-AppxPackage *picsart* | Remove-AppxPackage
+Get-AppxPackage *plex* | Remove-AppxPackage
+Get-AppxPackage *windowsstore* | Remove-AppxPackage
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Get-AppxPackage *bingweather* | Remove-AppxPackage
